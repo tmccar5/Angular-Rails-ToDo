36b99373869d726e1e471b0f735d4ba019c1a67e
tmccar5 <tommccarthyguitar@gmail.com>
2016-05-24 13:04:27 -0500

    basic form tinkering

--- app/assets/javascripts/post/postCtrl.js
+++ app/assets/javascripts/post/postCtrl.js
@@ -1,6 +1,31 @@
 angular
   .module('app')
   .controller('postCtrl', ['$scope', function($scope) {
-  $scope.greeting = 'Hola!';
+		$scope.greeting = '';
+		$scope.customGreeting = '';
+
+
+		$scope.english = function(){
+			$scope.greeting = "hello";
+		};
+
+		$scope.spanish = function(){
+			$scope.greeting = "hola'!";
+		};
+		$scope.greet = function(phrase){
+			$scope.greeting = phrase;
+		};
+		$scope.firstname = "";
+
+
+
+		$scope.names = ["tom","bob","sam"];
+
+		$scope.user = {name: ""};
+
+		$scope.save = function(name){
+			$scope.names.push(name);
+		};
+
 }]);
 
